5a7fa59c5ed01bf3026781a8277794678e0e6399
Carrak <51882360+Carrak@users.noreply.github.com>
2022-11-14 00:02:34 +0400

    Change origins

--- src/Program.cs
+++ src/Program.cs
@@ -13,12 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "cors", builder =>
     {
-        //for when you're running on localhost
-        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
-        .AllowAnyHeader().AllowAnyMethod();
-
-
-        //builder.WithOrigins("url from where you're trying to do the requests")
+        builder.WithOrigins("https://honkaihub.com", "https://www.honkaihub.com").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
