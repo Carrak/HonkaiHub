5e307e52c42a2c2540581e760df372b5aaab5ecf
Carrak <51882360+Carrak@users.noreply.github.com>
2022-11-17 00:43:08 +0400

    Use UTC for all dates

--- src/clientapp/src/index.tsx
+++ src/clientapp/src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import utc from 'dayjs/plugin/utc'
+import dayjs from 'dayjs'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+dayjs.extend(utc)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
