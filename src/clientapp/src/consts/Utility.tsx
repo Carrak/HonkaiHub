eb2fd4e4b3024acc5088c263dd03254ce4d97422
Carrak <51882360+Carrak@users.noreply.github.com>
2022-11-19 03:31:09 +0400

    Allow negatives

--- src/clientapp/src/consts/Utility.tsx
+++ src/clientapp/src/consts/Utility.tsx
@@ -1,12 +1,17 @@
 export const getNum = (prev: number | null, input: string, min?: number, max?: number): number | null => {
-    if (input === "")
+    if (input === "" || input === "-")
         return null;
 
+    if (input.includes("-", 1) && prev) 
+            return prev * -1;
+
     let num: number = parseInt(input);
-    if (isNaN(num) || typeof min === 'undefined' || num < min) {
-        console.log("returned prev " + prev)
+    if (isNaN(num)) {
         return prev;
     }
+
+    if (typeof min !== 'undefined' && num < min)
+        return min;
 
     if (typeof max !== 'undefined' && num > max)
         return max;
