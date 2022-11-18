f261e2e3da42bcd317230e49c7a77ce86d222cfc
Carrak <51882360+Carrak@users.noreply.github.com>
2022-11-19 03:29:38 +0400

    Fix birthdays
    Originally only worked for as far as one year

--- src/Services/BirthdaysService.cs
+++ src/Services/BirthdaysService.cs
@@ -5,53 +5,47 @@ namespace HonkaiHub.Services
 {
     public class BirthdaysService
     {
-        private IReadOnlyList<DateTime> _birthdays;
-        private int _lastCheckedDay;
         private IReadOnlyList<CharacterBirthday> _rawBirthdays;
 
         public BirthdaysService(IOptionsMonitor<CharacterBirthdaysOptions> birthdays)
         {
-            _lastCheckedDay = DateTime.UtcNow.Day;
             _rawBirthdays = birthdays.CurrentValue.List;
-            UpdateBirthdays();
 
             birthdays.OnChange(newBirthdays =>
             {
                 _rawBirthdays = newBirthdays.List;
-                UpdateBirthdays();
             });
-        }
-
-        private void UpdateBirthdays()
-        {
-            DateTime now = DateTime.UtcNow;
-            List<DateTime> birthdays = new();
-
-            foreach (var rawbd in _rawBirthdays)
-            {
-                var bd = new DateTime(now.Year, rawbd.Month, rawbd.Day);
-                if (now > bd)
-                    bd = new DateTime(now.Year + 1, rawbd.Month, rawbd.Day);
-
-                birthdays.Add(bd);
-            }
-
-            birthdays.Sort();
-            _birthdays = birthdays.AsReadOnly();
         }
 
         public int GetBirthdaysCount(DateTime from, DateTime to)
         {
-            if (_lastCheckedDay != DateTime.UtcNow.Day)
-            {
-                UpdateBirthdays();
-                _lastCheckedDay = DateTime.UtcNow.Day;
-            }
-
             int sum = 0;
-            foreach (var bd in _birthdays)
-                if (bd > from && bd <= to)
-                    sum++;
+
+            int yearDiff = to.Year - from.Year;
+            if (yearDiff == 0)
+            {
+                foreach (var rbd in _rawBirthdays)
+                {
+                    var date = new DateTime(from.Year, rbd.Month, rbd.Day);
+                    if (date > from && date <= to)
+                        sum++;
+                }
+            }
+            else
+            {
+                sum += (yearDiff - 1) * _rawBirthdays.Count;
+
+                foreach (var rbd in _rawBirthdays)
+                {
+                    var date1 = new DateTime(from.Year, rbd.Month, rbd.Day);
+                    if (date1 > from)
+                        sum++;
+
+                    var date2 = new DateTime(to.Year, rbd.Month, rbd.Day);
+                    if (date2 <= to)
+                        sum++;
+                }
+            }
 
             return sum;
         }
