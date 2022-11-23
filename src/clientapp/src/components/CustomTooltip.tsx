8c468217fa238b16decbbaf896f3acf1e5fa16c8
Carrak <51882360+Carrak@users.noreply.github.com>
2022-11-23 16:58:54 +0400

    Don't position tooltips at edge="end"

--- src/clientapp/src/components/CustomTooltip.tsx
+++ src/clientapp/src/components/CustomTooltip.tsx
@@ -36,7 +36,7 @@ class CustomTooltip extends Component<ICustomTooltipProps, ICustomTooltipState> 
                     disableTouchListener
                     title={this.props.tooltip}
                 >
-                    <IconButton edge="end" size="small" onClick={this.handleTooltipOpen}>
+                    <IconButton size="small" onClick={this.handleTooltipOpen}>
                         <HelpOutlineIcon className="tooltip-icon" />
                     </IconButton>
                 </Tooltip>
