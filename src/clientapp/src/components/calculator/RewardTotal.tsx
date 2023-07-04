f6a1c037ef4c3619745fd2bd995f0d2a3724ea2e
Carrak <51882360+Carrak@users.noreply.github.com>
2023-07-04 23:48:00 +0400

    Rework reward total
    Instead of a single category it has been split into two:
    1. raw output
    2. conversion to any selected currency

--- src/clientapp/src/components/calculator/RewardTotal.tsx
+++ src/clientapp/src/components/calculator/RewardTotal.tsx
@@ -1,57 +1,126 @@
 import { Grid, Stack } from "@mui/material";
 import { Component, Fragment } from "react";
 import { ICalculatorTotal } from "../Calculator";
+import { conversionCurrencies, select } from "../../consts/Dropdowns";
 import CustomTooltip from "../CustomTooltip";
+import { StyledTextField } from "./textfields/StyledTextField";
+
+const rewardTotalTooltip = "Reward total is your balance plus everything you earn across the specified period. " +
+    "It is calculated when all the required fields are filled. " +
+    "Card calculations also include the focused card you can buy from the BP shop."
+
+const currencyConverterTooltip = "Choose a currency to convert your crystals to. The converted amount is added to the existing amount, " +
+    "e. g. 1000 crystals and 10 focused cards would convert to 13 cards and 160 crystals. " +
+    "The rest of the rewards remain unchanged."
 
 interface IRewardTotalProps {
     totals: ICalculatorTotal
 }
 
-class RewardTotal extends Component<IRewardTotalProps> {
+interface IRewardTotalState {
+    conversionCurrency: number
+}
+
+const currencyPrices: number[] = [
+    1,   // crystals
+    280, // focused
+    280, // expansion
+    150, // elf
+    120, // sp
+    200  // dorm
+]
+
+const changedColor: string = "#7ee588"
+
+class RewardTotal extends Component<IRewardTotalProps, IRewardTotalState> {
+
+    constructor(props: IRewardTotalProps) {
+        super(props)
+
+        this.state = {
+            conversionCurrency: 1
+        }
+
+        this.onChangeCurrency = this.onChangeCurrency.bind(this);
+    }
+
+    onChangeCurrency(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        this.setState(() => ({ conversionCurrency: +e.target.value }))
+    }
 
     render() {
-        const tooltip = "Reward total is calculated when all the required fields are filled. " +
-            "Card calculations also include the card you can buy from the BP shop, your balance and custom rewards"
+        
+        let currencyValues: number[] = [
+            this.props.totals.grandTotal,
+            this.props.totals.focused,
+            this.props.totals.expansion,
+            this.props.totals.elf,
+            this.props.totals.sp,
+            this.props.totals.dorm
+        ]
+
+        let elements: JSX.Element[] = currencyValues.map(x => <span>{x}</span>);
+
+        let cc = this.state.conversionCurrency;
+        let oldValue = currencyValues[cc];
+
+        currencyValues[0] = this.props.totals.grandTotal % currencyPrices[cc];
+        currencyValues[cc] += + Math.floor(this.props.totals.grandTotal / currencyPrices[cc]);
+
+        if (oldValue != currencyValues[cc]) {
+            elements[0] = <b style={{ color: changedColor }} >{currencyValues[0]}</b>
+            elements[cc] = <b style={{ color: changedColor }}>{currencyValues[cc]}</b>
+        }
 
         return <Fragment>
-            <Grid className="reward-total-text" container direction="column" rowSpacing={1}>
+            <Grid className="reward-total-text" container direction="column" rowSpacing={0.6}>
                 <Grid item className="top-note">
-                    Reward total&nbsp;
-                    <CustomTooltip tooltip={tooltip} />
+                    Total&nbsp;
+                    <CustomTooltip tooltip={rewardTotalTooltip} />
                 </Grid>
                 <Grid item display="flex" justifyContent="center" alignItems="center">
-                    <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.grandTotal}
+                    <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.grandTotal}&nbsp;&nbsp;
                 </Grid>
-                <Grid item>
+                <Grid item display="flex" justifyContent="center" alignItems="center">
+                    <img src="Focused.webp" className="currency-icon" alt="focused cards" />{this.props.totals.focused}&nbsp;&nbsp;
+                    <img src="Expansion.webp" className="currency-icon" alt="expansion cards" />{this.props.totals.expansion}
+                </Grid>
+                <Grid item display="flex" justifyContent="center" alignItems="center">
+                    <img src="ELF.webp" className="currency-icon" alt="ELF cards" />{this.props.totals.elf}&nbsp;&nbsp;
+                    <img src="SP.webp" className="currency-icon" alt="SP cards" />{this.props.totals.sp}&nbsp;&nbsp;
+                    <img src="Dorm.webp" className="currency-icon" alt="dorm cards" />{this.props.totals.dorm}
+                </Grid>
+                <Grid item display="flex" justifyContent="center" alignItems="center">
+                </Grid>
+                <Grid item mt={1} mb={1}>
                     <Grid container direction="row" justifyContent="center">
-                        <Stack display="flex" flexDirection="column" spacing={1}> 
-                            <div className="flex">
-                                or &nbsp;
-                                <img src="Focused.webp" className="currency-icon" alt="focused cards" />{this.props.totals.focused} &nbsp;
-                                <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.focusedRemainder}
+                        <Stack display="flex" flexDirection="column" spacing={0.6}> 
+                            <div className="top-note">
+                                Convert crystals to&nbsp;
+                                <CustomTooltip tooltip={currencyConverterTooltip} />
                             </div>
-                            <div className="flex">
-                                or &nbsp;
-                                <img src="Expansion.webp" className="currency-icon" alt="expansion cards" />{this.props.totals.expansion} &nbsp;
-                                <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.expansionRemainder}
-                            </div>
-                            <div className="flex">
-                                or &nbsp;
-                                <img src="ELF.webp" className="currency-icon" alt="ELF cards" />{this.props.totals.elf} &nbsp;
-                                <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.elfRemainder}
-                            </div>
-                            <div className="flex">
-                                <Grid item className="reward-total-row">
-                                    or &nbsp;
-                                    <img src="SP.webp" className="currency-icon" alt="SP cards" />{this.props.totals.sp} &nbsp;
-                                    <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.spRemainder}
+                            <StyledTextField
+                                label=""
+                                value={this.state.conversionCurrency ?? ""}
+                                onChange={(e) => this.onChangeCurrency(e)}
+                                select
+                                id={"currency_converter"}>
+                                {select(conversionCurrencies)}
+                            </StyledTextField>
+                            <Grid className="reward-total-text" container direction="column" rowSpacing={0.6}>
+                                <Grid item display="flex" justifyContent="center" alignItems="center">
+                                    <img src="Crystals.webp" className="currency-icon" alt="crystals" />{elements[0]}
                                 </Grid>
-                            </div>
-                            <div className="flex">
-                                or &nbsp;
-                                <img src="Dorm.webp" className="currency-icon" alt="dorm cards" />{this.props.totals.dorm} &nbsp;
-                                <img src="Crystals.webp" className="currency-icon" alt="crystals" />{this.props.totals.dormRemainder}
-                            </div>
+                                <Grid item display="flex" justifyContent="center" alignItems="center">
+                                    <img src="Focused.webp" className="currency-icon" alt="focused cards" />{elements[1]}&nbsp;&nbsp;
+                                    <img src="Expansion.webp" className="currency-icon" alt="expansion cards" />{elements[2]}
+                                </Grid>
+                                <Grid item display="flex" justifyContent="center" alignItems="center">
+                                    <img src="ELF.webp" className="currency-icon" alt="ELF cards" />{elements[3]}&nbsp;&nbsp;
+                                    <img src="SP.webp" className="currency-icon" alt="SP cards" />{elements[4]}&nbsp;&nbsp;
+                                    <img src="Dorm.webp" className="currency-icon" alt="dorm cards" />{elements[5]}
+                                </Grid>
+                            </Grid> 
                         </Stack>
                     </Grid>
                 </Grid>
